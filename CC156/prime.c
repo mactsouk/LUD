0ed9e15b995f14eb08ad87983dab9e2a834a20b5
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2015-10-01 10:31:09 +0300

    Create prime.c

---
+++ CC156/prime.c
@@ -0,0 +1,55 @@
+// Programmer: Mihalis Tsoukalos
+// Date: Wednesday 10 June 2015
+//
+// Find prime numbers up to a limit
+// The limit is given as a command line argument
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int TOTAL = 0;
+
+int isPrime(int number)
+{
+    if (number == 1)
+        return 1;
+    
+    if (number == 2)
+        return 1;
+    
+    int i = 0;
+    for (i=2; i<number; i++)
+    {
+        TOTAL++;
+        if (number % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(int argc, char** argv)
+{
+    int i = 0;
+    int total = 0;
+    
+    // Get the integer as string and convert it to integer
+    int limit = atoi(argv[1]);
+
+    // Find prime numbers
+    for (i = 3; i<=limit; i++)
+    {
+        TOTAL++;
+        if (isPrime(i))
+        {
+            total++;
+        }
+    }
+    
+    printf("Found %d primes from 3 up to %d.\n", total, limit);
+    printf("Total number of computations: %d.\n", TOTAL);
+    
+    return 0;
+}
